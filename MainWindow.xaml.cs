adb25dbb6fdd4fc59343f37cccd6ac12a2d516f0
Unknown <varujik10@gmail.com>
2018-06-30 00:05:08 +0400

    First Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace ER_W
+{
+    public partial class MainWindow : Window
+    {
+        // Options
+        public static bool isDrawEntity = false;
+        public bool isConnectionEnabled = false;
+        //
+        public int entitiesIDCounter = 0;
+        public int relationsIDCounter = 0;
+        public List<Entity> entities = new List<Entity>();
+        //
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.RelationPropertiesVisibility(Visibility.Hidden);
+            this.EntityPropertiesVisibility(Visibility.Hidden);
+            Entity.Canvas = canvas;
+            Entity.Window = mainWindow;
+
+            Relation.Canvas = canvas;
+            Relation.Window = mainWindow;
+
+            //Relation.f(new Point(450, 50), new Point(700, 100));
+            //var LineTwo = new Line
+            //{
+            //    Stroke = Brushes.Black,
+            //    StrokeThickness = 2,
+            //    Fill = Brushes.Transparent,
+            //    X1 = 450,
+            //    Y1 = 50,
+            //    X2 = 700,
+            //    Y2 = 100
+            //};
+            //Relation r = new ER_W.Relation(null, null, 1);
+            //Function f = r.GetLinearFunction(new Point(400, 50), new Point(700, 50));
+            //MessageBox.Show(f.K + " : " + f.B);
+            //Point B = r.GetPointCoordinatesOnDistance(f.K, f.B, new Point(400, 500), 100);
+
+            //label1.Content = B.X + " : " + B.Y;
+            //canvas.Children.Add(LineTwo);
+        }
+
+        private void createEntityBtn_Click(object sender, RoutedEventArgs e)
+        {
+            isDrawEntity = true;
+        }
+
+        private void connectEntityBtn_Click(object sender, RoutedEventArgs e)
+        {
+            isConnectionEnabled = true;
+        }
+
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (isDrawEntity)
+            {
+                Entity entity = new Entity(e.GetPosition(canvas).X, e.GetPosition(canvas).Y, entitiesIDCounter++, Brushes.Aqua);
+                entities.Add(entity);
+                isDrawEntity = false;
+            }
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            //canvas.MinWidth = mainWindow.Width;
+            //canvas.MinHeight = mainWindow.Height;
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            label.Content = e.GetPosition(canvas).X + " : " + e.GetPosition(canvas).Y;
+            if (Entity.SelectedEntity != null && Entity.SelectedEntity.isMoveEntity)
+            {
+                Entity.SelectedEntity.Move(e.GetPosition(canvas).X, e.GetPosition(canvas).Y);
+            }
+            if (Relation.SelectedRelation != null && Relation.SelectedRelation.isMoveConnection)
+            {
+                Relation.SelectedRelation.Move(e.GetPosition(canvas).X, e.GetPosition(canvas).Y);
+            }
+        }
+
+        private void comboBoxFirstEntity_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Entity newEntity = comboBoxFirstEntity.SelectedItem as Entity;
+            if (Relation.ChangeableRelation != null && newEntity != null)
+            {
+                Entity oldEntity = Relation.ChangeableRelation.FirstEntity;
+                
+                oldEntity.Relations.Remove(Relation.ChangeableRelation);
+                newEntity.Relations.Add(Relation.ChangeableRelation);
+                //
+                Relation.ChangeableRelation.FirstEntity = newEntity;
+                Relation.ChangeableRelation.UpdateConnection();
+            }
+        }
+
+        private void comboBoxSecondEntity_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Entity newEntity = comboBoxSecondEntity.SelectedItem as Entity;
+            if (Relation.ChangeableRelation != null && newEntity != null)
+            {
+                Entity oldEntity = Relation.ChangeableRelation.SecondEntity;
+
+                oldEntity.Relations.Remove(Relation.ChangeableRelation);
+                newEntity.Relations.Add(Relation.ChangeableRelation);
+                //
+                Relation.ChangeableRelation.SecondEntity = newEntity;
+                Relation.ChangeableRelation.UpdateConnection();
+            }
+        }
+
+        public void RelationPropertiesVisibility(Visibility type)
+        {
+            comboBoxFirstEntity.Visibility = type;
+            comboBoxSecondEntity.Visibility = type;
+
+            cardinalNumberEntityOneLabel.Visibility = type;
+            cardinalNumberEntityTwoLabel.Visibility = type;
+
+            cardinalNumberEntityOneCombobox.Visibility = type;
+            cardinalNumberEntityTwoCombobox.Visibility = type;
+
+            relationTypeLabel.Visibility = type;
+            relationTypeCombobox.Visibility = type;
+
+            firstEntityLabel.Visibility = type;
+            secondEntityLabel.Visibility = type;
+        }
+
+        public void EntityPropertiesVisibility(Visibility type)
+        {
+            entityNameLabel.Visibility = type;
+            entityNameTxtbox.Visibility = type;
+
+            
+        }
+
+        private void entityNameTxtbox_KeyUp(object sender, KeyEventArgs e)
+        {
+            Entity.ChangeableEntity.ChangeEntityName(entityNameTxtbox.Text);
+        }
+
+        private void cardinalNumberEntityOneCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.UpdateCardinalNumber(sender as ComboBox, ref Relation.ChangeableRelation.EntityOneCardinalOne, ref Relation.ChangeableRelation.EntityOneCardinalZero);
+        }
+
+        private void cardinalNumberEntityTwoCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.UpdateCardinalNumber(sender as ComboBox, ref Relation.ChangeableRelation.EntityTwoCardinalOne, ref Relation.ChangeableRelation.EntityTwoCardinalZero);
+        }
+        public void UpdateCardinalNumber(ComboBox combobox, ref Line line, ref Ellipse circle)
+        {
+            string cardinalNumber;
+                cardinalNumber = (combobox.SelectedItem as Label).Content.ToString();
+            if (line != null)
+                canvas.Children.Remove(line);
+            if (circle != null)
+                canvas.Children.Remove(circle);
+            switch (cardinalNumber)
+            {
+                case "One":
+                    line = new Line();
+                    circle = null;
+                    canvas.Children.Add(line);
+                    break;
+                case "Zero":
+                    line = null;
+                    circle = new Ellipse();
+                    canvas.Children.Add(circle);
+                    break;
+                case "None":
+                    line = null;
+                    circle = null;
+                    break;
+            }
+            Relation.ChangeableRelation.UpdateConnection();
+        }
+
+        private void relationTypeCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox combobox = sender as ComboBox;
+            Relation.ChangeableRelation.ChangeRelationType((combobox.SelectedItem as Label).Content.ToString());
+        }
+    }
+}
---
+++ Relation.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ER_W
+{
+    public class Relation : Shape
+    {
+        private const int rhombusWidth = 100;
+        private const int rhombusHeight = 100;
+        public const int labelWidth = rhombusWidth / 2 - 15;
+        public const int labelHeight = (rhombusHeight / 4);
+        private Label TypeLabel;
+        private RelationType Type { get; set; }
+        public static Relation SelectedRelation { get; set; }
+        public static Relation ChangeableRelation { get; set; }
+
+        public Entity FirstEntity { get; set; }
+        public Entity SecondEntity { get; set; }
+        public int RelationID { get; set; }
+
+        Line LineOne { get; set; }
+        Line LineTwo { get; set; }
+
+        public Line EntityOneCardinalOne;
+        public Line EntityTwoCardinalOne; // used
+
+        public Ellipse EntityOneCardinalZero; // used
+        public Ellipse EntityTwoCardinalZero;
+
+        Image RhombusImage { get; set; }
+
+        public bool isMoveConnection = false;
+
+        public Relation(Entity FE, Entity SE, int ID)
+        {
+            try
+            {
+                FirstEntity = FE;
+                SecondEntity = SE;
+                this.RelationID = ID;
+                //this.Name = "1:N";//"Relation" + this.RelationID;
+                ConnectEntities(FE.PositionX, FE.PositionY, SE.PositionX, SE.PositionY);
+
+                RhombusImage.MouseDown += RhombusImage_MouseDown;
+                RhombusImage.MouseUp += RhombusImage_MouseUp;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void RhombusImage_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Relation.SelectedRelation = null;
+            this.isMoveConnection = false;
+        }
+
+        private void RhombusImage_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Relation.ChangeableRelation = this;
+            if (e.LeftButton == System.Windows.Input.MouseButtonState.Pressed)
+            {
+                Relation.SelectedRelation = this;
+                this.isMoveConnection = true;
+
+                Window.comboBoxFirstEntity.Items.Clear();
+                Window.comboBoxSecondEntity.Items.Clear();
+                foreach (Entity entity in Window.entities)
+                {
+                    Window.comboBoxFirstEntity.Items.Add(entity);
+                    Window.comboBoxSecondEntity.Items.Add(entity);
+                }
+                Window.comboBoxFirstEntity.SelectedItem = this.FirstEntity;
+                Window.comboBoxSecondEntity.SelectedItem = this.SecondEntity;
+                Window.relationTypeCombobox.SelectedIndex = 4;
+                if (this.EntityOneCardinalOne != null)
+                    Window.cardinalNumberEntityOneCombobox.SelectedIndex = 0;
+                else if (this.EntityOneCardinalZero != null)
+                    Window.cardinalNumberEntityOneCombobox.SelectedIndex = 1;
+                else
+                    Window.cardinalNumberEntityOneCombobox.SelectedIndex = 2;
+
+                if (this.EntityTwoCardinalOne != null)
+                    Window.cardinalNumberEntityTwoCombobox.SelectedIndex = 0;
+                else if (this.EntityTwoCardinalZero != null)
+                    Window.cardinalNumberEntityTwoCombobox.SelectedIndex = 1;
+                else
+                    Window.cardinalNumberEntityTwoCombobox.SelectedIndex = 2;
+
+                Window.EntityPropertiesVisibility(Visibility.Hidden);
+                Window.RelationPropertiesVisibility(Visibility.Visible);
+            }
+        }
+
+        public void ConnectEntities(double X1, double Y1, double X2, double Y2)
+        {
+            RhombusImage = new Image();
+            RhombusImage.Source = new BitmapImage(new Uri(@"pack://application:,,,/Resources/rhombus.png"));
+            RhombusImage.Width = rhombusWidth;
+            RhombusImage.Height = rhombusHeight;
+            RhombusImage.Stretch = Stretch.Fill;
+            PositionX = (FirstEntity.PositionX + SecondEntity.PositionX) / 2;
+            PositionY = (FirstEntity.PositionY + SecondEntity.PositionY) / 2;
+
+
+
+            LineOne = new Line
+            {
+                Stroke = Brushes.Black,
+                StrokeThickness = 2,
+                Fill = Brushes.Transparent,
+                X1 = FirstEntity.PositionX,
+                Y1 = FirstEntity.PositionY,
+                X2 = PositionX,
+                Y2 = PositionY + rhombusHeight / 2
+            };
+            LineTwo = new Line
+            {
+                Stroke = Brushes.Black,
+                StrokeThickness = 2,
+                Fill = Brushes.Transparent,
+                X1 = SecondEntity.PositionX,
+                Y1 = SecondEntity.PositionY,
+                X2 = PositionX + rhombusWidth,
+                Y2 = PositionY + rhombusHeight / 2
+            };
+            TypeLabel = new Label
+            {
+                Content = "",
+                Width = labelWidth,
+                Height = labelHeight,
+                //Background = new SolidColorBrush(Colors.Transparent)
+            };
+            Canvas.SetLeft(RhombusImage, PositionX);
+            Canvas.SetTop(RhombusImage, PositionY);
+            Canvas.SetLeft(TypeLabel, PositionX + labelWidth);
+            Canvas.SetTop(TypeLabel, PositionY + labelHeight - labelHeight / 2);
+            Canvas.Children.Add(RhombusImage);
+            Canvas.Children.Add(LineOne);
+            Canvas.Children.Add(LineTwo);
+            Canvas.Children.Add(TypeLabel);
+
+            this.UpdateConnection();
+        }
+
+        public void UpdateConnection()
+        {
+            Point pointOne = GetClosestCoordinates(FirstEntity.PositionX, FirstEntity.PositionY, PositionX, PositionY + rhombusHeight / 2);
+            Point pointTwo = GetClosestCoordinates(SecondEntity.PositionX, SecondEntity.PositionY, PositionX + rhombusWidth, PositionY + rhombusHeight / 2);
+
+            LineOne.X1 = pointOne.X;
+            LineOne.Y1 = pointOne.Y;
+
+
+            LineOne.X2 = PositionX;
+            LineOne.Y2 = PositionY + rhombusHeight / 2;
+
+            LineTwo.X1 = pointTwo.X;
+            LineTwo.Y1 = pointTwo.Y;
+
+            LineTwo.X2 = PositionX + rhombusWidth;
+            LineTwo.Y2 = PositionY + rhombusHeight / 2;
+
+            DrawCardinalOne(new Point(LineTwo.X1, LineTwo.Y1), new Point(LineTwo.X2, LineTwo.Y2), ref EntityOneCardinalOne);
+            DrawCardinalOne(new Point(LineOne.X1, LineOne.Y1), new Point(LineOne.X2, LineOne.Y2), ref EntityTwoCardinalOne);
+
+            DrawCardinalZero(new Point(LineTwo.X1, LineTwo.Y1), new Point(LineTwo.X2, LineTwo.Y2), ref EntityOneCardinalZero);
+            DrawCardinalZero(new Point(LineOne.X1, LineOne.Y1), new Point(LineOne.X2, LineOne.Y2), ref EntityTwoCardinalZero);
+        }
+
+        public void Move(double X, double Y)
+        {
+            this.PositionX = X - rhombusWidth / 2;
+            this.PositionY = Y - rhombusHeight / 2;
+            Canvas.SetLeft(Relation.SelectedRelation.RhombusImage, PositionX);
+            Canvas.SetTop(Relation.SelectedRelation.RhombusImage, PositionY);
+            Canvas.SetLeft(this.TypeLabel, PositionX + labelWidth);
+            Canvas.SetTop(this.TypeLabel, PositionY + labelHeight - labelHeight / 2);
+
+            this.UpdateConnection();
+        }
+
+        public Point GetClosestCoordinates(double entityX, double entityY, double rhombusX, double rhombusY)
+        {
+            Point point = new Point();
+            if (entityX >= rhombusX && entityY >= rhombusY)
+            {
+                point.X = entityX;
+                point.Y = entityY;
+            }
+            if (entityX <= rhombusX && entityY >= rhombusY)
+            {
+                point.X = entityX + Entity.entityWidth;
+                point.Y = entityY;
+            }
+            if (entityX <= rhombusX && entityY <= rhombusY)
+            {
+                point.X = entityX + Entity.entityWidth;
+                point.Y = entityY + Entity.entityHeight;
+            }
+            if (entityX >= rhombusX && entityY <= rhombusY)
+            {
+                point.X = entityX;
+                point.Y = entityY + Entity.entityHeight;
+            }
+            return point;
+        }
+        public void DrawCardinalOne(Point p1, Point p2, ref Line line)
+        {
+            if (line == null)
+                return;
+
+            Point midpoint = new Point();
+
+            // Vipovot sawyisi wrfe. (y2 - y1) / (x2 - x1) = k
+            double k = (double)(p2.Y - p1.Y) / (double)(p2.X - p1.X);
+
+            // Perpendikularuli wrfis k
+            if (k != 0)
+                k = -1 / k;
+
+            midpoint.X = (p1.X + p2.X) / 2;
+            midpoint.Y = (p1.Y + p2.Y) / 2;
+
+            // martobulis b
+            double b = -k * midpoint.X + midpoint.Y;
+
+            List<Point> points = this.GetTwoCoordinatesOnDistance(midpoint.X, k, b, 10);
+
+            line.Stroke = Brushes.Black;
+            line.StrokeThickness = 2;
+            line.Fill = Brushes.Transparent;
+            line.X1 = points[0].X;
+            line.Y1 = points[0].Y;
+            line.X2 = points[1].X;
+            line.Y2 = points[1].Y;
+        }
+        public List<Point> GetTwoCoordinatesOnDistance(double x1, double k, double b, double distance)
+        {
+            double D = 4 * x1 * x1 - 4 * (x1 * x1 - (distance * distance / (1 + k * k)));
+
+            double X1 = (2 * x1 + Math.Sqrt(D)) / 2;
+            double Y1 = k * X1 + b;
+
+            double X2 = (2 * x1 - Math.Sqrt(D)) / 2;
+            double Y2 = k * X2 + b;
+
+            var res = new List<Point>
+            {
+                new Point(X1,Y1),
+                new Point(X2,Y2)
+            };
+            return res;
+        }
+        public void DrawCardinalZero(Point p1, Point p2, ref Ellipse circle)
+        {
+            if (circle == null)
+                return;
+            Point midpoint = new Point();
+
+            midpoint.X = (p1.X + p2.X) / 2 - 15;
+            midpoint.Y = (p1.Y + p2.Y) / 2 - 15;
+
+            circle.Stroke = Brushes.Black;
+            circle.StrokeThickness = 2;
+            circle.Width = 30;
+            circle.Height = 30;
+            Canvas.SetLeft(circle, midpoint.X);
+            Canvas.SetTop(circle, midpoint.Y);
+        }
+        //public void ChangeRelationName(string newName)
+        //{
+        //    this.Name = newName;
+        //    this.NameLabel.Content = newName;
+        //}
+        public void ChangeRelationType(string newType)
+        {
+            switch (newType)
+            {
+                case "1:1":
+                    this.Type = RelationType.OneOne;
+                    break;
+                case "1:N":
+                    this.Type = RelationType.OneN;
+                    break;
+                case "N:1":
+                    this.Type = RelationType.NOne;
+                    break;
+                case "N:M":
+                    this.Type = RelationType.NM;
+                    break;
+            }
+            if (newType != "")
+                this.TypeLabel.Content = this.GetRelationTypeString(this.Type);
+        }
+
+        public string GetRelationTypeString(RelationType type)
+        {
+            switch (type)
+            {
+                case RelationType.OneOne:
+                    return "1:1";
+                case RelationType.OneN:
+                    return "1:N";
+                case RelationType.NOne:
+                    return "N:1";
+                case RelationType.NM:
+                    return "N:M";
+            }
+            return "";
+        }
+    }
+}
